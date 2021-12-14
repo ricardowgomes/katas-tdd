ddfbf58a366ced84da81eb7604dfa26b9c804b31
ricardowgomes <ricardowgomes@outlook.com>
2021-12-13 19:03:57 -0700

    first attempt

--- supermarket/kata.js
+++ supermarket/kata.js
@@ -3,32 +3,36 @@
 // When you buy 4 you get one free
 
 const products = [{
-  id: 1,
-  name: 'Coke',
-  type: 'drinks',
-  quant: 5,
-  price: 2.05
+  1: {
+    name: 'Coke',
+    type: 'drinks',
+    quant: 8,
+    price: 2.05
+  }
 },
 {
-  id: 2,
-  name: 'Guarana',
-  type: 'drinks',
-  quant: 2,
-  price: 2.35
+  2: {
+    name: 'Guarana',
+    type: 'drinks',
+    quant: 4,
+    price: 2.35
+  }
 },
 {
-  id: 3,
-  name: 'Cookies',
-  type: 'snack',
-  quant: 8,
-  price: 0.99
+  3: {
+    name: 'Cookies',
+    type: 'snack',
+    quant: 8,
+    price: 0.99
+  }
 },
 {
-  id: 4,
-  name: 'Egg Roll',
-  type: 'food',
-  quant: 12,
-  price: 1.98
+  4: {
+    name: 'Egg Roll',
+    type: 'food',
+    quant: 12,
+    price: 1.98
+  }
 }]
 
 // 3 Cokes, 2 Guaranas, 1 Cookie, 1 Egg Roll
@@ -40,16 +44,53 @@ const shoppingList2 = [1, 1, 1, 2, 2, 3, 4, 4, 4, 4, 4];
 // 3 Cokes, 2 Guaranas, 6 Cookies, 1 Egg Roll
 const shoppingList3 = [1, 1, 1, 1, 2, 3, 3, 3, 3, 3, 3, 4];
 
+let message;
+
 const shopping = (shoppingList) => {
+  const shoppingCart = [...products];
   const total = 0;
 
+  shoppingList.map(item => {
+    console.log("X-----------------------X", item)
+    console.log(shoppingCart[item])
+    console.log("X-----------------------X")
+    if (shoppingCart[item]) {
+      products[item].quant -= 1
 
+      if (products[item].quant < 1) {
+        message = `*${products[item].name} out of stock! Not processed`
+      }
+
+      if (shoppingCart[item]['amount']) {
+        shoppingCart[item]['amount'] += 1
+
+      } else {
+        shoppingCart[item]['amount'] = 1
+      }
+
+      if (shoppingCart[item]['subtotal']) {
+        if (shoppingCart[item]['subtotal'] === shoppingCart[item]['subtotal'] * 3 && shoppingCart[item]['type'] === 'drinks') {
+          shoppingCart[item]['subtotal'] = 5
+
+        } else {
+          shoppingCart[item]['subtotal'] += shoppingCart[item]['subtotal']
+        }
+
+      } else {
+        shoppingCart[item]['subtotal'] = shoppingCart[item].price
+      }
+    }
+  })
 
   return total;
 }
 
 const change = (total, money) => {
+  let change = 0;
 
+
+
+  return change;
 }
 
 // 1st case with sufficient funds
@@ -62,13 +103,30 @@ const shopping2 = change(shopping(shoppingList2), 50)
 const shopping3 = change(shopping(shoppingList3), 50)
 
 // 1st case with insufficient funds
-const shopping1 = change(shopping(shoppingList1), 10)
-
-// 2nd case with insufficient funds
-const shopping2 = change(shopping(shoppingList2), 10)
-
-// 3rd case with insufficient funds
-const shopping3 = change(shopping(shoppingList3), 10)
+const shopping4 = change(shopping(shoppingList1), 10)
 
 console.log("-----------------------")
-console.log("-----------------------")
+console.log("SHOPPING 01")
+console.log("$50 BUYING 3 Cokes, 2 Guaranas, 1 Cookie, 1 Egg Roll")
+console.log(shopping1)
+console.log(message)
+// console.log("-----------------------")
+// console.log("SHOPPING 02")
+// console.log("$50 BUYING 3 Cokes, 2 Guaranas, 1 Cookie, 5 Egg Roll")
+// console.log(shopping2)
+// console.log(message)
+// console.log("-----------------------")
+// console.log("SHOPPING 03")
+// console.log("$50 BUYING 3 Cokes, 2 Guaranas, 6 Cookies, 1 Egg Roll")
+// console.log(shopping3)
+// console.log(message)
+// console.log("-----------------------")
+// console.log("SHOPPING 04")
+// console.log("$50 BUYING 3 Cokes, 2 Guaranas, 1 Cookie, 1 Egg Roll")
+// console.log(shopping4)
+// console.log(message)
+// console.log("-----------------------")
+// console.log("STOCK")
+// console.log("Total sales: 9 Cokes, 6 Guaranas, 8 Cookies, 7 Egg Roll")
+// console.log(products)
+// console.log("-----------------------")
